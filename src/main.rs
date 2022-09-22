aca1c22f833b34cb58446ea5ad33d00d9c71fc1b
Muhammad Zaryab Rafique <muhammadzaryabrafique@gmail.com>
2022-09-22 17:58:15 +0500

    Module system in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+
+async fn hello() -> impl Responder{
+    HttpResponse::Ok().json("Hello from rust and mongoDB")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()>{
+    HttpServer::new(|| App::new().service(hello))
+        .bind(("localhost", 8080))?
+        .run()
+        .await
 }
